8e48ce5b8b9f26be9c119da758efd8fc5fe0f21b
Tegacreatives <contact@tegacreatives.com>
2024-01-15 00:36:54 +0100

    Setup for vfx

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,8 +6,15 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField]
     private float speed = 100;
+
+    [SerializeField]
+    private float lifetime = 20.0f;
+
+    private float timeLastSmoke;
+    private float pointsWorth = 1;
     private bool isMoving = false;
 
+    public float PointsWorth { get => pointsWorth; set => pointsWorth = value; }
     public bool IsMoving { get => isMoving; set => isMoving = value; }
     // Start is called before the first frame update
     void Start()
@@ -20,6 +27,18 @@ public class Rocket : MonoBehaviour
     {
         if (isMoving)
         {
+            pointsWorth += (Time.deltaTime) * 10;
+            lifetime -= Time.deltaTime;
+            if (lifetime < 0f)
+            {
+                Destroy(gameObject);
+            }
+
+            if (Time.deltaTime >= timeLastSmoke + 0.04f)
+            {
+                timeLastSmoke = Time.time;
+
+            }
             transform.position += transform.forward * (speed * Time.deltaTime);
             transform.Rotate(new Vector3(0, 30 * Time.deltaTime, 0));
         }
