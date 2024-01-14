35ef32d0c0c7f853c990561d528429a23e17f628
Tegacreatives <contact@tegacreatives.com>
2024-01-14 22:30:36 +0100

    Initial Setup

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 100;
+    private bool isMoving = false;
+
+    public bool IsMoving { get => isMoving; set => isMoving = value; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
